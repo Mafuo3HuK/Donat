06bd044853f44e9d1b69db6e7543471e812c4aa0
Mafuo3HuK <kerjackov.sochi2014@yandex.ru>
2019-10-10 19:02:33 +0300

    First commit

---
+++ Donate Tester/Assets/Scripts/Number of bonuses.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Numberofbonuses : MonoBehaviour
+{
+    public TMP_Text Text;
+
+    private int _number;
+
+    public void Purchase1Pressed()
+    {
+        _number++;
+        Text.text = _number.ToString();
+    }
+}   
